37163a8e5c9d4a3ebec35550ed0b877b07944886
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-04 16:40:59 +0700

    rename props to xSpace and ySpace

--- src/lib/Grid.js
+++ src/lib/Grid.js
@@ -7,14 +7,14 @@ const Grid = styled("div")`
   > * {
     flex-basis: ${props => 100 / props.itemsPerRow}%;
     flex-grow: 1;
-    max-width: ${props => 100 / props.itemsPerRow - props.horizontalSpace}%;
-    ${props => `margin-bottom: ${props.verticalSpace}px`};
+    max-width: ${props => 100 / props.itemsPerRow - props.xSpace}%;
+    ${props => `margin-bottom: ${props.ySpace}px`};
   }
 `;
 Grid.defaultProps = {
   itemsPerRow: 4,
-  horizontalSpace: 1,
-  verticalSpace: 20
+  xSpace: 1,
+  ySpace: 20
 };
 
 export default Grid;
